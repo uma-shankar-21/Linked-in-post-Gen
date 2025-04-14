85b5209549a708d3be1d568aebf8f4ce17acc181
Uma Shankar Kommireddy <119053276+uma-shankar-21@users.noreply.github.com>
2025-04-14 14:11:10 +0530

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -1,30 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
-import os
-from dotenv import load_dotenv
+#import os
+#from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-
-
-# In[12]:
-
-
-load_dotenv()
-llm=ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"),model="llama-3.2-90b-vision-preview")
-
-
-# In[13]:
-
+GROQ_API_KEY="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"
+#load_dotenv()
+llm=ChatGroq(groq_api_key=GROQ_API_KEY,model="llama-3.2-90b-vision-preview")
 
 if __name__=="__main__":
     response=llm.invoke("What is difference between GenAI and LLM")
     print(response.content)
-
-
-# In[ ]:
-
-
-
-
