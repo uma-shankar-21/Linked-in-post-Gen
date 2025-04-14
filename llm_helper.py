b54ea6915e5664976a95de8db497d081a2ffc868
Uma Shankar Kommireddy <119053276+uma-shankar-21@users.noreply.github.com>
2025-04-14 14:15:58 +0530

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -1,6 +1,6 @@
 #import os
 #from dotenv import load_dotenv
-from langchain_groq import ChatGroq
+#from langchain_groq import ChatGroq
 GROQ_API_KEY="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"
 #load_dotenv()
 llm=ChatGroq(groq_api_key=GROQ_API_KEY,model="llama-3.2-90b-vision-preview")
